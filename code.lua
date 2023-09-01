e1c82e96f437863ffc4d88e098a13e75b139a2db
TheAio <76646135+TheAio@users.noreply.github.com>
2023-09-01 14:23:53 +0200

    Start update script at --update flag

--- code.lua
+++ code.lua
@@ -3,11 +3,20 @@ local Code = require "code.Code"
 local function printUsage()
   printError("Usage:")
   printError("  " .. arg[0] .. " <filename>")
+  printError("  " .. arg[0] .. " --update")
+end
+
+local function update()
+  print("Please wait...")
+  shell.run("wget run https://raw.githubusercontent.com/Possseidon/cc-code/main/code/update.lua")
 end
 
 local args = { ... }
 if #args ~= 1 then
   printUsage()
+  return
+elseif args[1] == "--update"
+  update()
   return
 end
 
